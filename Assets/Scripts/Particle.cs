cad172416ce4cfca2041793f41d950ae1cbc6d1f
Henri <henri.besancenotel@gmail.com>
2025-05-16 09:18:53 +0200

    Particle properties

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class Particle : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Initialize SPH particle state variables
+        public float mass = 1.0f;
+        public float density = 0.0f;
+        public float pressure = 0.0f;
+        public Vector3 velocity = Vector3.zero;
+        public Vector3 force = Vector3.zero;
+        public Vector3 position;
+        
     void Start()
     {
-        
+        return;
     }
 
     // Update is called once per frame
